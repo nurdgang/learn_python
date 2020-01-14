f7b74827073bf4952ac8bbd5126180ef18608427
nurdgang <58235468+nurdgang@users.noreply.github.com>
2020-01-14 09:25:50 -0700

    Update scatterplot.py

--- scatterplot.py
+++ scatterplot.py
@@ -1,8 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
 
 x = np.random.normal(1, 20, 250)
 y = np.random.normal(1, 20, 250)
 
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+line_of_best_fit = list(map(myfunc, x)
+
+def lineEq(x)
+  return slope * x + intercept
+                        
 plt.scatter(x, y)
+plt.plot(x, line_of_best_fit)
 plt.show()
+
+
